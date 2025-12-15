9f52c3ff9d56a15e6dc2bced2a4366c4a325767c
Asuka-Aozora <azdin1411@gmail.com>
2025-12-15 23:07:04 +0700

    feat: integrate Poppins font and enhance ProjectCard styles for improved typography and visuals

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,13 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/layout/navbar";
 import { Footer } from "@/components/layout/footer";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +25,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="id">
-      <body className={inter.className}>
+      <body className={`${inter.className} ${poppins.variable}`}>
         <Navbar />
         {children}
         <div className="flex items-center justify-center p-10">
