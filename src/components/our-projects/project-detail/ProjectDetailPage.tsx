668949df3a36f6950eb182e1493dfcf980ea5c03
Asuka-Aozora <azdin1411@gmail.com>
2026-01-06 22:27:34 +0700

    feat: implement ProjectGalleryContainer and ModalGallery for enhanced image viewing experience

--- src/components/our-projects/project-detail/ProjectDetailPage.tsx
+++ src/components/our-projects/project-detail/ProjectDetailPage.tsx
@@ -9,10 +9,11 @@ import {
   ChevronLeft,
   ChevronRight,
 } from "lucide-react";
-import ModalGallery from "../../common/ModalGallery";
+import ModalGallery from "./sections/gallery/ModalGallery";
 import SurroundingsGrid from "../../surroundings/SurroundingGrid";
 import Link from "next/link";
 import { ProjectEuy } from "@/app/projects/[slug]/page";
+import { ProjectGalleryContainer } from "./sections/gallery/ProjectGallery.container";
 
 type ModalSource = "gallery" | "houseType" | null;
 
@@ -47,117 +48,7 @@ const ProjectDetailPage = ({ project }: { project: ProjectEuy }) => {
         </div>
 
         {/* Image Gallery */}
-        <div className="relative overflow-hidden mb-8">
-          {/* SLIDER */}
-          <div
-            className="flex transition-transform duration-500 ease-out"
-            style={{
-              transform: `translateX(-${slideIndex * 100}%)`,
-            }}
-          >
-            {Array.from({ length: totalSlides }).map((_, slide) => {
-              const start = slide * 5;
-              const bigImage =
-                slide === 0 ? project.mainImage : project.gallery[start - 1];
-
-              const thumbs =
-                slide === 0
-                  ? project.gallery.slice(0, 4)
-                  : project.gallery.slice(start, start + 4);
-
-              return (
-                <div
-                  key={slide}
-                  className="min-w-full grid grid-cols-1 lg:grid-cols-2 gap-2 px-1  "
-                >
-                  {/* BIG IMAGE – 50% */}
-                  <div
-                    className="relative rounded-sm overflow-hidden group cursor-pointer"
-                    onClick={() => {
-                      setModalImages(images.map(cleanImageUrl));
-                      setActiveIndex(slide === 0 ? 0 : start);
-                      setModalSource("gallery");
-                      setIsModalOpen(true);
-                    }}
-                  >
-                    <img
-                      src={cleanImageUrl(bigImage)}
-                      className="w-full h-96 lg:h-[680px] object-cover transition-transform duration-500 group-hover:scale-[1.02]"
-                    />
-
-                    {/* overlay */}
-                    <div
-                      className="
-                      absolute inset-0
-                      bg-black/0
-                      group-hover:bg-black/30
-                      transition-colors duration-300
-                    "
-                    />
-                  </div>
-
-                  {/* THUMB GRID – 50% */}
-                  <div className="grid grid-cols-2 grid-rows-2 gap-2 h-96 lg:h-[680px]">
-                    {thumbs.map((img, i) => (
-                      <div
-                        key={i}
-                        className="relative overflow-hidden rounded-sm group cursor-pointer"
-                        onClick={() => {
-                          setModalImages(images.map(cleanImageUrl));
-                          setActiveIndex(slide === 0 ? i + 1 : start + i);
-                          setModalSource("gallery");
-                          setIsModalOpen(true);
-                        }}
-                      >
-                        <img
-                          src={cleanImageUrl(img)}
-                          className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-[1.05]"
-                        />
-
-                        <div
-                          className="
-      absolute inset-0
-      bg-black/0
-      group-hover:bg-black/25
-      transition-colors duration-300
-    "
-                        />
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-
-          {/* LEFT ARROW */}
-          {slideIndex > 0 && (
-            <button
-              onClick={() => setSlideIndex((i) => i - 1)}
-              className="group absolute left-4 top-1/2 -translate-y-1/2 
-        bg-white/70 backdrop-blur 
-        p-3 rounded-full shadow-md
-        hover:bg-white hover:scale-110 hover:shadow-xl
-        transition-all duration-300"
-            >
-              <ChevronLeft className="w-5 h-5 text-gray-800 group-hover:text-black" />
-            </button>
-          )}
-
-          {/* RIGHT ARROW */}
-          {slideIndex < totalSlides - 1 && (
-            <button
-              onClick={() => setSlideIndex((i) => i + 1)}
-              className="group absolute right-4 top-1/2 -translate-y-1/2 
-        bg-white/70 backdrop-blur 
-        p-3 rounded-full shadow-md
-        hover:bg-white hover:scale-110 hover:shadow-xl
-        transition-all duration-300"
-            >
-              <ChevronRight className="w-5 h-5 text-gray-800 group-hover:text-black" />
-            </button>
-          )}
-        </div>
+        <ProjectGalleryContainer project={project} />
 
         {/* Location Info */}
         <div className="bg-gray-50 rounded-2xl p-6 mb-8 flex items-start justify-between">
