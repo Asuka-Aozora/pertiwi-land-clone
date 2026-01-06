600d7fcc578ea95bf1026e5fa5d10fd5998bd17f
Asuka-Aozora <azdin1411@gmail.com>
2026-01-06 22:45:43 +0700

    feat: add ProjectLocation component to display project location details

--- src/components/our-projects/project-detail/ProjectDetailPage.tsx
+++ src/components/our-projects/project-detail/ProjectDetailPage.tsx
@@ -15,6 +15,7 @@ import Link from "next/link";
 import { ProjectGalleryContainer } from "./sections/gallery/ProjectGallery.container";
 import { ProjectEuy } from "./types";
 import ProjectHeader from "./sections/ProjectHeader";
+import ProjectLocation from "./sections/ProjectLocation";
 
 type ModalSource = "gallery" | "houseType" | null;
 
@@ -41,22 +42,7 @@ const ProjectDetailPage = ({ project }: { project: ProjectEuy }) => {
         <ProjectGalleryContainer project={project} />
 
         {/* Location Info */}
-        <div className="bg-gray-50 rounded-2xl p-6 mb-8 flex items-start justify-between">
-          <div className="flex-1">
-            <h3 className="text-xl font-bold text-gray-900 mb-2">
-              {project.location}
-            </h3>
-            <p className="text-gray-600 mb-1">{project.fullAddress}</p>
-          </div>
-          <button className="text-blue-600 flex items-center gap-2 hover:underline">
-            <MapPin className="w-5 h-5" />
-            <Link
-              href={`https://www.google.com/maps/search/?api=1&query=${project.location}`}
-            >
-              Lihat Map
-            </Link>
-          </button>
-        </div>
+        <ProjectLocation project={project} />
 
         {/* Description */}
         <div className="mb-12">
