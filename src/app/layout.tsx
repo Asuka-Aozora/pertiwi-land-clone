ca0cea718e6f83102e4b30f65cde112ca4f61088
Asuka-Aozora <azdin1411@gmail.com>
2026-01-12 08:47:48 +0700

    fixbug: change to new GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,24 +32,11 @@ export default function RootLayout({
     <html lang="id">
       <head>
         {/* Google Tag Manager */}
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17783455508"
-          id="gtm-script"
-          strategy="afterInteractive"
-        >{`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'AW-17783455508');
-          (function(w,d,s,l,i){w[l]=w[l]||[];
-          w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-          var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-          j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;
-          f.parentNode.insertBefore(j,f);
+        <Script id="gtm-script" strategy="afterInteractive">{`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-TRQD8MKD');
         `}</Script>
         {/* End Google Tag Manager */}
