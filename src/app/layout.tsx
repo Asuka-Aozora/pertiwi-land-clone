f50a9e72c33be58a9a15965430d6fbbf84462b92
Asuka-Aozora <azdin1411@gmail.com>
2026-01-15 20:07:44 +0700

    feat: Migrates Google Tag Manager to `@next/third-parties`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { Navbar } from "@/components/layout/navbar";
 import { Footer } from "@/components/layout/footer";
 import { Poppins } from "next/font/google";
-import Script from "next/script";
+import { GoogleTagManager } from "@next/third-parties/google";
 import WhatsAppFloating from "@/components/common/WhatsAppFloating";
 
 const poppins = Poppins({
@@ -30,29 +30,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="id">
-      <head>
-        {/* Google Tag Manager */}
-        <Script id="gtm-script" strategy="afterInteractive">{`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-TRQD8MKD');
-        `}</Script>
-        {/* End Google Tag Manager */}
-      </head>
+      <GoogleTagManager gtmId="GTM-TRQD8MKD" />
 
       <body className={`${inter.className} ${poppins.variable}`}>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TRQD8MKD"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
         <Navbar />
         {children}
         <div className="flex items-center justify-center p-10">
