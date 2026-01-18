0f97558863c2703f35788a54d78e62d89b64e7c4
Asuka-Aozora <azdin1411@gmail.com>
2026-01-19 00:41:52 +0700

    refactor: update Supabase key usage to `NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY`.

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -4,7 +4,7 @@ export function createClient() {
   // Create a supabase client on the browser with project's credentials
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
     {
       auth: {
         autoRefreshToken: false,
